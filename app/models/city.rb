2792b3141de98866d9d5f93de8c6dd4c47dd0679
David Mowrer <david.mowrer@comcast.net>
2014-07-25 11:24:45 -0600

    Changes to add validation to the models

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,4 +1,5 @@
 class City < ActiveRecord::Base
+
   validates :elevation,
             numericality: {
               greater_than_or_equal_to: 3315,
@@ -6,5 +7,9 @@ class City < ActiveRecord::Base
               message: 'is too low, the lowest elevation in Colorado is 3,315 feet'
             }
   
-  validates :name, presence: true
+  validates :name, :state, :elevation, :population, presence: true
+  validates :state, length: { is: 2 }
+  validates :elevation, :population, numericality: true
+  validates :population, numericality: { greater_than: 0 }
+
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,10 @@
 class Person < ActiveRecord::Base
-  validates :first_name, presence: true
+
+  validates :first_name, :last_name, :address_line_one, :city, presence: true
+  validates_format_of :first_name, :last_name, :with => /[a-z]/
+  validates :zip_code, length: { minimum: 5 }
+  validates :zip_code, numericality: true
+  validates :state, length: { is: 2 }
+  validates :email, uniqueness: true, :allow_blank => true
+
 end
