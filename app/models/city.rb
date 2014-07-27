399a6dc1568f9189eb21423b2b6398a2b3f1a17e
David Mowrer <david.mowrer@comcast.net>
2014-07-26 18:17:40 -0600

    Additional validations

--- app/models/city.rb
+++ app/models/city.rb
@@ -6,9 +6,12 @@ class City < ActiveRecord::Base
               if: lambda { |city| city.state == 'CO' },
               message: 'is too low, the lowest elevation in Colorado is 3,315 feet'
             }
-  
   validates :name, :state, :elevation, :population, presence: true
   validates :state, length: { is: 2 }
+  validates :state, format: { with: /\A[A-Z]+\z/,
+                              message: "only allows 2 Capital letters" }
+  validates :state, inclusion: { in: %w(AK AZ AR CA CO CT DE DC FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI WY),
+                                 message: "field only allows US States" }
   validates :elevation, :population, numericality: true
   validates :population, numericality: { greater_than: 0 }
 
--- app/models/person.rb
+++ app/models/person.rb
@@ -4,7 +4,11 @@ class Person < ActiveRecord::Base
   validates_format_of :first_name, :last_name, :with => /[a-z]/
   validates :zip_code, length: { minimum: 5 }
   validates :zip_code, numericality: true
-  validates :state, length: { is: 2 }
   validates :email, uniqueness: true, :allow_blank => true
+  alidates :state, length: { is: 2 }
+  validates :state, format: { with: /\A[A-Z]+\z/,
+                              message: "only allows 2 Capital letters" }
+  validates :state, inclusion: { in: %w(AK AZ AR CA CO CT DE DC FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI WY),
+                                 message: "field only allows US States" }
 
 end
